6256f4a41eb1740949d5f346571287bfc083425b
Nathan Patel <30420600+baconsenpaii@users.noreply.github.com>
2022-02-26 17:07:01 +0000

    add another view for user pages

--- flaskr/users.py
+++ flaskr/users.py
@@ -10,13 +10,31 @@ from flaskr.db import get_db
 bp = Blueprint("users", __name__, url_prefix="/")
 
 
-@bp.route("/user/<int:uid>")
-def user_view(uid: int):
+@bp.route("/user/id/<int:uid>")
+def user_view_id(uid: int):
     db = get_db()
     error = None
 
     user = db.execute(
         'SELECT id, username, password, perms, content FROM Users WHERE id = ?', (uid,)
+    ).fetchone()
+
+    if user is None:
+        error = "User doesn't exist!"
+    
+    if error is not None:
+        flash(error)
+
+    return render_template("user.html", user=user)
+
+
+@bp.route("/user/<string:uname>")
+def user_view_name(uname: str):
+    db = get_db()
+    error = None
+
+    user = db.execute(
+        'SELECT id, username, password, perms, content FROM Users WHERE username = ?', (uname,)
     ).fetchone()
 
     if user is None:
