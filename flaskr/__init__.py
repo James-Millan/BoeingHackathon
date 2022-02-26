68b278efbad81a0ed48970ea17ca02c3b790f8b4
baconsenpaii <baconforevery2@gmail.com>
2022-02-26 17:03:19 +0000

    add user pages

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,4 +36,7 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import users
+    app.register_blueprint(users.bp)
+
     return app
--- flaskr/users.py
+++ flaskr/users.py
@@ -10,4 +10,19 @@ from flaskr.db import get_db
 bp = Blueprint("users", __name__, url_prefix="/")
 
 
-@bp.route("/user/<>")
+@bp.route("/user/<int:uid>")
+def user_view(uid: int):
+    db = get_db()
+    error = None
+
+    user = db.execute(
+        'SELECT id, username, password, perms, content FROM Users WHERE id = ?', (uid,)
+    ).fetchone()
+
+    if user is None:
+        error = "User doesn't exist!"
+    
+    if error is not None:
+        flash(error)
+
+    return render_template("user.html", user=user)
