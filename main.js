e00654102a29e99250ff51b95af9b71b70a88a81
eo20203 <78903047+eo20203@users.noreply.github.com>
2022-02-27 08:15:24 +0000

    coloured map

--- main.js
+++ main.js
@@ -1,34 +1,132 @@
-import GeoJSON from "ol/format/GeoJSON";
-import Map from "ol/Map";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import View from "ol/View";
-import { Fill, Style } from "ol/style";
-
-const style = new Style({
-  fill: new Fill({
+const style = new ol.Style({
+  fill: new ol.Fill({
     color: "#eeeeee"
   })
 });
 
-const vectorLayer = new VectorLayer({
-  background: "#000075",
-  source: new VectorSource({
+const element = document.getElementById("popup");
+var geoLayer = new ol.VectorLayer({
+  background: "#0066cc",
+  source: new ol.VectorSource({
     url: "https://openlayers.org/data/vector/ecoregions.json",
-    format: new GeoJSON()
+    format: new ol.GeoJSON()
   }),
+  //selects each polygon from the GeoJSON and fills it with the relevant colour
   style: function (feature) {
-    const color = feature.get("COLOR_NNH") || "#eeeeee";
+    const color =
+      feature.get("COLOR" + document.getElementById("mapType").value) ||
+      "#eeeeee";
     style.getFill().setColor(color);
     return style;
   }
 });
 
-const map = new Map({
-  layers: [vectorLayer],
+var map = new ol.Map({
+  layers: [geoLayer],
   target: "map",
-  view: new View({
+  view: new ol.View({
     center: [0, 0],
-    zoom: 1
+    zoom: 2
   })
+});
+
+// popup overlay for longitude and latitude
+const popup = new ol.Overlay({
+  element: element,
+  positioning: "bottom-center",
+  stopEvent: false
+});
+map.addOverlay(popup);
+
+// white outline to contrast black text
+const outlineStyle = new ol.Style({
+  stroke: new ol.Stroke({
+    color: "rgba(255, 255, 255, 1)",
+    width: 1
+  })
+});
+
+// layer for outlines
+const locationLayer = new ol.VectorLayer({
+  source: new ol.VectorSource(),
+  map: map,
+  style: outlineStyle
+});
+
+//mouse popup tracker
+let outline;
+map.on("pointermove", function (evt) {
+  if (evt.dragging) {
+    return;
+  }
+  geoLayer.getFeatures(evt.pixel).then(function (features) {
+    // functionally an if else statement with the syntax
+    // feature = features[0] if features.length, else undefined
+    const feature = features.length ? features[0] : undefined;
+    if (feature) {
+      popup.setPosition(evt.coordinate);
+      // popover jquery
+      $(element).popover({
+        placement: "top",
+        html: true,
+        //gets value from GeoJSON
+        content: feature.get(document.getElementById("dataType").value)
+      });
+      $(element).popover("show");
+    }
+    // refreshes popup every 300 ticks
+    setTimeout(function () {
+      $(element).popover("dispose");
+    }, 300);
+    //outlines hovered over regions
+    if (feature !== outline) {
+      if (feature) {
+        locationLayer.getSource().addFeature(feature);
+      }
+      if (outline) {
+        locationLayer.getSource().removeFeature(outline);
+      }
+
+      outline = feature;
+    }
+  });
+
+  //displays requested data after clicking
+  map.on("click", function (evt) {
+    // removes previous elemnt after click
+    $(element).popover("dispose");
+    const info = function (pixel) {
+      geoLayer.getFeatures(pixel).then(function (features) {
+        // functionally an if else statement with the syntax
+        // feature = features[0] if features.length, else undefined
+        const feature = features.length ? features[0] : undefined;
+        // displays information beneath map
+        const info = document.getElementById("info");
+        if (feature) {
+          info.innerHTML =
+            // name of ecological region
+            feature.get("ECO_NAME") +
+            ", " +
+            // name of biome
+            feature.get("BIOME_NAME") +
+            ", " +
+            //name of zoographical realm
+            feature.get("REALM") +
+            " realm: " +
+            //nature needs half classification
+            feature.get("NNH_NAME");
+          popup.setPosition(evt.coordinate);
+          $(element).popover({
+            placement: "top",
+            html: true,
+            content: feature.get(document.getElementById("dataType").value)
+          });
+          $(element).popover("show");
+        } else {
+          info.innerHTML = "&nbsp;";
+        }
+      });
+    };
+    info(map.getEventPixel(evt.originalEvent));
+  });
 });
